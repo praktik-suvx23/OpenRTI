fb72a7783df84bcb306e25a0ffdab2ab305c2018
ojonss <sven.oskar.jonsson@gmail.com>
2025-01-16 13:12:13 +0100

    Add initial implementation of MyFederate with CMake configuration and source files

---
+++ ownOpenRTI/build/CMakeFiles/MyFederate.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyFederate.dir/src/main.cpp.o"
+  "CMakeFiles/MyFederate.dir/src/main.cpp.o.d"
+  "MyFederate"
+  "MyFederate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyFederate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
