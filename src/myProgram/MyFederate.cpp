cf1fa3eee3156b55dab2888b73b45f3c5d684e7c
ojonss <sven.oskar.jonsson@gmail.com>
2025-02-14 14:26:44 +0100

    Add delay after receiving sync point in MyFederate

--- src/myProgram/MyFederate.cpp
+++ src/myProgram/MyFederate.cpp
@@ -377,6 +377,7 @@ void startSubscriber(int instance) {
             rtiAmbassador->evokeMultipleCallbacks(0.1, 1.0);
         }
         std::wcout << L"MyFederate received sync point." << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
 
         // Get handles and subscribe to object class attributes
         federateAmbassador->objectClassHandle = rtiAmbassador->getObjectClassHandle(L"HLAobjectRoot.robot");
