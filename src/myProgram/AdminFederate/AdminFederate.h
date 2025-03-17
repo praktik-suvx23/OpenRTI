c294af58be0c127af15aa5ee6a654dd654d86d3a
ojonss <sven.oskar.jonsson@gmail.com>
2025-03-17 08:32:17 +0100

    Refactor AdminFederate class to improve structure and add simulation setup methods

--- src/myProgram/AdminFederate/AdminFederate.h
+++ src/myProgram/AdminFederate/AdminFederate.h
@@ -4,16 +4,11 @@
 #include "AdminFederateAmbassador.h"
 
 class AdminFederate {
-
 public:
-    std::unique_ptr<rti1516e::RTIambassador> rtiAmbassador;
-    std::unique_ptr<AdminFederateAmbassador> federateAmbassador;
-
-    std::wstring syncPoint = L"ReadyToRun";
-
     AdminFederate();
     ~AdminFederate();
     void runFederate();
+    
 private:
     void createRTIambassador();
     void connectToRTI();
@@ -21,7 +16,21 @@ private:
     void joinFederation();
     void registerSyncPoint();
     void achiveSyncPoint();
+    void initializeHandles();
+    void publishInteractions();
+    void setupSimulation();
+    void publishSetupSimulationInteraction(int teamA, int teamB, double timeScaleFactor);
+    void registerSyncSimulationSetupComplete();
+    void adminLoop();
     void resignFederation();
-};
 
+    std::unique_ptr<rti1516e::RTIambassador> rtiAmbassador;
+    std::unique_ptr<AdminFederateAmbassador> federateAmbassador;
+
+    std::wstring federateName = L"AdminFederate";
+    std::wstring federationName = L"robotFederation";
+    std::vector<std::wstring> fomModules = {L"foms/robot.xml"};
+    std::wstring minModule = L"foms/MIM.xml";
+};
+    
 #endif
