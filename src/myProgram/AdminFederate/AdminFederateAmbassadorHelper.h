f34e3c0859a6a3685057be2d28fd05ad8d35e8e8
ojonss <sven.oskar.jonsson@gmail.com>
2025-03-17 08:32:32 +0100

    Add getter and setter methods for simulation setup parameters in Ambassador classes

--- src/myProgram/AdminFederate/AdminFederateAmbassadorHelper.h
+++ src/myProgram/AdminFederate/AdminFederateAmbassadorHelper.h
@@ -7,11 +7,29 @@ class AmbassadorGetter {
 public:
     static std::wstring getSyncLabel(AdminFederateAmbassador& ambassador) {
         return ambassador.syncLabel;}
+    
+    static rti1516e::InteractionClassHandle getSetupSimulationHandle(AdminFederateAmbassador& ambassador) {
+        return ambassador.setupSimulationHandle;}
+    static rti1516e::ParameterHandle getBlueShipsParam(AdminFederateAmbassador& ambassador) {
+        return ambassador.blueShipsParam;}
+    static rti1516e::ParameterHandle getRedShipsParam(AdminFederateAmbassador& ambassador) {
+        return ambassador.redShipsParam;}
+    static rti1516e::ParameterHandle getTimeScaleFactorParam(AdminFederateAmbassador& ambassador) {
+        return ambassador.timeScaleFactorParam;}
 };
 
 class AmbassadorSetter {
 public:
     static void setSyncLabel(AdminFederateAmbassador& ambassador, std::wstring label) {
         ambassador.syncLabel = label;}
+
+    static void setSetupSimulationHandle(AdminFederateAmbassador& ambassador, rti1516e::InteractionClassHandle handle) {
+        ambassador.setupSimulationHandle = handle;}
+    static void setBlueShipsParam(AdminFederateAmbassador& ambassador, rti1516e::ParameterHandle handle) {
+        ambassador.blueShipsParam = handle;}
+    static void setRedShipsParam(AdminFederateAmbassador& ambassador, rti1516e::ParameterHandle handle) {
+        ambassador.redShipsParam = handle;}
+    static void setTimeScaleFactorParam(AdminFederateAmbassador& ambassador, rti1516e::ParameterHandle handle) {
+        ambassador.timeScaleFactorParam = handle;}
 };
 #endif
