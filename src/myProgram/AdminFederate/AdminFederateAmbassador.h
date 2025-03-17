8bf54329136d571657308c490d6b0ff288d27f62
ojonss <sven.oskar.jonsson@gmail.com>
2025-03-17 08:32:27 +0100

    Add interaction class and parameter handles for simulation setup in AdminFederateAmbassador

--- src/myProgram/AdminFederate/AdminFederateAmbassador.h
+++ src/myProgram/AdminFederate/AdminFederateAmbassador.h
@@ -4,6 +4,7 @@
 #include <RTI/RTIambassadorFactory.h>
 #include <RTI/RTIambassador.h>
 #include <RTI/NullFederateAmbassador.h>
+#include <RTI/encoding/BasicDataElements.h>
 #include <iostream>
 
 class AmbassadorGetter;
@@ -12,10 +13,15 @@ class AmbassadorSetter;
 class AdminFederateAmbassador : public rti1516e::NullFederateAmbassador {
     friend class AmbassadorGetter;
     friend class AmbassadorSetter;
-    
+
     rti1516e::RTIambassador* _rtiAmbassador;
 
     std::wstring syncLabel = L"";
+
+    rti1516e::InteractionClassHandle setupSimulationHandle; // Interaction class handle for setupSimulation interaction
+    rti1516e::ParameterHandle blueShipsParam;               // Parameter handle for blueShips parameter
+    rti1516e::ParameterHandle redShipsParam;                // Parameter handle for redShips parameter
+    rti1516e::ParameterHandle timeScaleFactorParam;         // Parameter handle for timeScaleFactor parameter
 public:
     AdminFederateAmbassador(rti1516e::RTIambassador* rtiAmbassador);
     ~AdminFederateAmbassador();
