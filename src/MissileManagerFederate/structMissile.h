86f6623f1202881bdec41c2938a0423ad60fe036
ojonss <oskar.jonsson@chasacademy.se>
2025-03-24 12:55:02 +0100

    Enhance Missile struct with additional attributes for target tracking and initialization

--- src/MissileManagerFederate/structMissile.h
+++ src/MissileManagerFederate/structMissile.h
@@ -4,21 +4,31 @@
 #include <RTI/RTI1516.h>
 #include <string>
 
+// This should be based on the FOM: MissileFlight ..?
+
 struct Missile {
     rti1516e::ObjectInstanceHandle objectInstanceHandle;
     std::wstring structMissileID;
     std::wstring structMissileTeam;
     std::pair<double, double> structMissilePosition;
+    std::pair<double, double> structInitialTargetPosition;
     double structMissileAltitude;
     double structMissileSpeed;
+    std::chrono::time_point<std::chrono::high_resolution_clock> structMissileStartTime;
+    double structInitialBearing;
+    double structMissileDistanceToTarget;
 
     Missile(rti1516e::ObjectInstanceHandle objectInstanceHandle)
         : objectInstanceHandle(objectInstanceHandle),
         structMissileID(L""),
         structMissileTeam(L""),
         structMissilePosition(std::make_pair(0.0, 0.0)),
+        structInitialTargetPosition(std::make_pair(0.0, 0.0)),
         structMissileAltitude(0.0),
-        structMissileSpeed(0.0) {}
+        structMissileSpeed(0.0),
+        structMissileStartTime(std::chrono::high_resolution_clock::now()),
+        structInitialBearing(0.0),
+        structMissileDistanceToTarget(0.0) {}
 };
 
 #endif
