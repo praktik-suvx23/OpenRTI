7f3af030cad590a89472ce73c6b79e35adf685f9
ojonss <oskar.jonsson@chasacademy.se>
2025-04-03 09:30:22 +0200

    Update Missile ID format in simulation output

--- src/MissileManagerFederate/MissileFederate.cpp
+++ src/MissileManagerFederate/MissileFederate.cpp
@@ -353,7 +353,7 @@ void MissileFederate::runSimulationLoop() {
                 std::vector<std::wstring> finalData = {
                     L"--------------------------------------------",
                     L"Instance : " + std::to_wstring(missile.objectInstanceHandle.hash()),
-                    L"Missile ID : " + missile.structMissileID + L" (" + missile.structMissileTeam + L")",
+                    L"Missile ID : Missile" + std::to_wstring(missile.structMissileID) + L" (" + missile.structMissileTeam + L")",
                     L"Target ID : " + missile.targetShipID,
                     L"Last Distance : " + std::to_wstring(missile.structMissileDistanceToTarget) + L" meters",
                     L"Last Altitude : " + std::to_wstring(missile.structMissileAltitude) + L" meters",
