fea03df10c02da84f38d3a924281d96d469d2eb6
ojonss <oskar.jonsson@chasacademy.se>
2025-04-08 09:32:39 +0200

    Add synchronization points and time regulation methods in AdminFederateAmbassador

--- src/AdminFederate/AdminFederateAmbassador.cpp
+++ src/AdminFederate/AdminFederateAmbassador.cpp
@@ -17,4 +17,33 @@ void AdminFederateAmbassador::announceSynchronizationPoint (
         std::wcout << L"Master Federate synchronized at SimulationSetupComplete." << std::endl;
         syncLabel = label;
     }
+    if (label == L"AdminReady") {
+        std::wcout << L"Master Federate synchronized at AdminReady." << std::endl;
+        syncLabel = label;
+    }
+    if (label == L"RedShipReady") {
+        std::wcout << L"Master Federate synchronized at ShipReady." << std::endl;
+        syncLabel = label;
+    }
+    if (label == L"EveryoneReady") {
+        std::wcout << L"Master Federate synchronized at EveryoneReady." << std::endl;
+        syncLabel = label;
+    }
+}
+
+void AdminFederateAmbassador::timeRegulationEnabled(const rti1516e::LogicalTime& theFederateTime) {
+    isRegulating = true;
+    std::wcout << L"Time Regulation Enabled: " << theFederateTime << std::endl;
+}
+
+void AdminFederateAmbassador::timeConstrainedEnabled(const rti1516e::LogicalTime& theFederateTime) {
+    isConstrained = true;
+    std::wcout << L"Time Constrained Enabled: " << theFederateTime << std::endl;
+}
+
+void AdminFederateAmbassador::timeAdvanceGrant(const rti1516e::LogicalTime& theTime) {
+    std::wcout << L"[DEBUG] Time Advance Grant received: "
+               << dynamic_cast<const rti1516e::HLAfloat64Time&>(theTime).getTime() << std::endl;
+
+    isAdvancing = false;  // Allow simulation loop to continue
 }
