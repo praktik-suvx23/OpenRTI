ef90bb3508f502af5c65c41c4149d9689d71edd2
ojonss <oskar.jonsson@chasacademy.se>
2025-04-14 15:47:02 +0200

    Prepared file with basic setup.

---
+++ src/VisualRepresentation/CommunicationWithPython/PyLink.h
@@ -0,0 +1,54 @@
+#ifndef PYLINK_H
+#define PYLINK_H
+
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+#include <chrono>
+#include <vector>
+#include <map>
+
+#include "PyLinkAmbassador.h"
+#include "../SendData.cpp"
+
+class PyLink {
+public:
+    PyLink();
+    ~PyLink();
+    void runFederate();
+    
+private:
+    void createRTIambassador();
+    void connectToRTI();
+    void initializeFederation();
+    void joinFederation();
+    void initializeHandles();
+    void publishAttributes();
+    void subscribeAttributes();
+    void initializeTimeFactory();
+    void enableTimeManagement();
+    void socketsSetup();
+    void readyCheck();
+    void adminLoop();
+    void resignFederation();
+
+    std::unique_ptr<rti1516e::RTIambassador> rtiAmbassador;
+    std::unique_ptr<PyLinkAmbassador> federateAmbassador;
+
+    rti1516e::HLAfloat64TimeFactory* logicalTimeFactory = nullptr;
+
+    std::wstring federateName = L"PyLink";
+    std::wstring federationName = L"robotFederation";
+    std::vector<std::wstring> fomModules = {L"foms/FOM.xml"};
+    std::wstring minModule = L"foms/MIM.xml";
+
+    double timeScaleFactor;
+
+    int blueship_socket;
+    int redship_socket;
+    int heartbeat_socket;
+};
+    
+#endif
