44413a3e27e1cfa675daaf663c004827ff56c9aa
ojonss <oskar.jonsson@chasacademy.se>
2025-05-12 13:33:33 +0200

    Refactor InitialHandshake structure to correct distance type and enhance hash function implementation

--- src/AdminFederate/tempStruct.h
+++ src/AdminFederate/tempStruct.h
@@ -23,7 +23,38 @@ struct InitialHandshake {
     std::wstring targetID;
     int32_t maxMissilesRequired;
     int32_t missilesCurrentlyTargeting;
-    int32_t distanceToTarget;
+    double distanceToTarget;
+
+    bool operator==(const InitialHandshake& other) const {
+        return shooterID == other.shooterID &&
+               missilesLoaded == other.missilesLoaded &&
+               targetID == other.targetID &&
+               maxMissilesRequired == other.maxMissilesRequired &&
+               missilesCurrentlyTargeting == other.missilesCurrentlyTargeting &&
+               distanceToTarget == other.distanceToTarget;
+    }
 };
+
+inline void hash_combine(std::size_t& seed, std::size_t value) {
+    seed ^= value + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+namespace std {
+    template <>
+    struct hash<InitialHandshake> {
+        std::size_t operator()(const InitialHandshake& hs) const {
+            std::size_t seed = 0;
+            hash_combine(seed, std::hash<std::wstring>()(hs.shooterID));
+            hash_combine(seed, std::hash<int32_t>()(hs.missilesLoaded));
+            hash_combine(seed, std::hash<std::wstring>()(hs.targetID));
+            hash_combine(seed, std::hash<int32_t>()(hs.maxMissilesRequired));
+            hash_combine(seed, std::hash<int32_t>()(hs.missilesCurrentlyTargeting));
+            hash_combine(seed, std::hash<double>()(hs.distanceToTarget));
+            return seed;
+        }
+    };
+}
+
+
 
 #endif // TEMPSTRUCT_H
