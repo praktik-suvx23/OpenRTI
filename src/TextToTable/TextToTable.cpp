0d046f0574687fc2a62b4dfad07783e76a056183
dragodror <philipjonsson02@gmail.com>
2025-06-05 11:16:37 +0200

    added output of ships being created to write to finalData before simulation start.
    Added TextToTable Folder aswell as CmakeList for that

---
+++ src/TextToTable/TextToTable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <unordered_map>
+#include <iomanip>
+#include <algorithm>
+
+
+struct TableData {
+    double timeFlying;
+};
+
+int main() {
+    int8_t numberOfMissiles;
+    int8_t numberOfTargets;
+}
