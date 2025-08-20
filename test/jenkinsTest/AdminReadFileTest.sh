9c30a26686d585ecbc551dda4198505164879e60
dragodror <philipjonsson02@gmail.com>
2025-08-20 13:47:34 +0200

    added admin text file test

---
+++ test/jenkinsTest/AdminReadFileTest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
+BUILD_DIR="$PROJECT_ROOT/build"
+# Start admin and provide inputs from the existing text file
+
+./admin < "$SCRIPT_DIR/input/admin_inputs.txt"
