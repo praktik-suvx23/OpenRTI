6fc024c02a91b645be2f2d7a52fc485857b49a6f
dragodror <philipjonsson02@gmail.com>
2025-08-20 14:16:29 +0200

    changed filepath to be dynamic

--- test/jenkinsTest/AdminReadFileTest.sh
+++ test/jenkinsTest/AdminReadFileTest.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PROJECT_ROOT="$(cd "$SCRIPT_DIR/../.." && pwd)"
-BUILD_DIR="$PROJECT_ROOT/build"
-INPUT_DIR="$SCRIPT_DIR/input"
-LOG_DIR="$SCRIPT_DIR/logs"
-PYTHON_DIR="$PROJECT_ROOT/src/VisualRepresentation"
-# Start admin and provide inputs from the existing text file
 
-./admin < "../test/input/dummy_input_admin.txt"
+
+SCRIPT_DIR="$(dirname "$0")"
+ADMIN_INPUT_FILE_SHORTCUT="$SCRIPT_DIR/dummy_input_admin.txt"
+./admin < "$ADMIN_INPUT_FILE_SHORTCUT"
 
 sleep 5
 
