83e51a189dc2160ca863c94b907576078d83521e
dragodror <philipjonsson02@gmail.com>
2025-08-20 13:59:16 +0200

    changed file path

--- test/jenkinsTest/AdminReadFileTest.sh
+++ test/jenkinsTest/AdminReadFileTest.sh
@@ -2,6 +2,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
 BUILD_DIR="$PROJECT_ROOT/build"
+ADMIN_INPUT="$PROJECT_ROOT/test/input/dummy_input_admin.txt"
 # Start admin and provide inputs from the existing text file
 
-./admin < "$SCRIPT_DIR/input/dummy_input_admin.txt"
+./admin < "$ADMIN_INPUT"
